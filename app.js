0186db886fd470a489e8f82e9fb7a139cdcffd0b
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-30 22:38:23 -0400

    Updates navToggle

--- app.js
+++ app.js
@@ -6,3 +6,7 @@
 
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    console.log(links.classList)
+})
